2a799a9c52995e700758551ad9820fe0396621b3
anuradhakrsna <anuradhakrsna18@gmail.com>
2017-08-11 22:46:57 -0400

    CORS - allow post and delete
    CORS - allow post and delete

--- server/MovieFlix/src/main/java/io/egen/MovieFlix/MyConfiguration.java
+++ server/MovieFlix/src/main/java/io/egen/MovieFlix/MyConfiguration.java
@@ -14,7 +14,8 @@ public class MyConfiguration {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**")
+				.allowedMethods("PUT", "DELETE", "GET", "POST");
 			}
 		};
 	}
